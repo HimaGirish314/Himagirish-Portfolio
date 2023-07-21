b41d29d16789df6cb22a7888067dfd11fbcef810
Himagirish <Himagirish.bandi@gmail.com>
2023-07-21 11:07:02 +0530

    configured reac-router-dom with responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Navbar from "./components/Navbar";
+import { Routes,Route } from "react-router-dom";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
       <Navbar />
+        <Routes>
+          <Route path="/"  element={<Home />}/>
+          <Route path="/about"  element={<About />}/>
+          <Route path="/projects"  element={<Projects />}/>
+          <Route path="/contact"  element={<Contact />}/>
+      </Routes>
+     
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {AiOutlineCloseCircle} from 'react-icons/ai'
 const Navbar = () => {
+  const [clicked,setClicked]=useState(false)
   return (
         <nav>
-            <h3 className='logo'>Himagirish.dev</h3>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
+            <Link to='/'><h3 className='logo'>Himagirish.dev</h3></Link>
+              <div className="mobile-menu" onClick={()=>{setClicked(!clicked)}}>
+                {clicked ?<AiOutlineCloseCircle className='menu-icons' />:<GiHamburgerMenu className='menu-icons' />}
+              </div>
+            <ul className={clicked ?"nav-items":"nav-items close"}>
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/projects"><li>Projects</li></Link>
+                <Link to="/contact"><li>Contact</li></Link>
             </ul>
-      
         </nav>
   )
 }
